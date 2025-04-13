64c81f8b6de4e65ef9f08751940cd270c4cd27ff
zoli0829 <zoli0829@gmail.com>
2025-04-13 08:57:08 +0100

    Initial commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Zoltan Vegh on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
